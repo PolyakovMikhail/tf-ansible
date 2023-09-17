182de6304f246c88151ae43cab4a06fc2360f591
PolyakovMikhail <pronik24@gmail.com>
2023-09-17 19:10:20 +0300

    new file:   main.tf

---
+++ main.tf
@@ -0,0 +1,167 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+  zone = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-ctrl" {
+  name = "vm-ctrl"
+  hostname="vm-ctrl"
+  resources {
+    cores  = 1
+    memory = 0.5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80le4b8gt2u33lvubr"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-ha" {
+  name = "vm-ha"
+  hostname = "vm-ha"
+  resources {
+    cores  = 1
+    memory = 0.5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80le4b8gt2u33lvubr"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-backend1" {
+  name = "vm-backend1"
+  hostname = "vm-backend1"
+  resources {
+    cores  = 1
+    memory = 0.5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80le4b8gt2u33lvubr"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-backend2" {
+  name = "vm-backend2"
+  hostname = "vm-backend2"
+  resources {
+    cores  = 1
+    memory = 0.5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80le4b8gt2u33lvubr"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-backend3" {
+  name = "vm-backend3"
+  hostname = "vm-backend3"
+  resources {
+    cores  = 1
+    memory = 0.5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80le4b8gt2u33lvubr"
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+  metadata = {
+    ssh-keys = "centos:${file("~/.ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_ctrl" {
+  value = yandex_compute_instance.vm-ctrl.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_ha" {
+  value = yandex_compute_instance.vm-ha.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_backend1" {
+  value = yandex_compute_instance.vm-backend1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_backend2" {
+  value = yandex_compute_instance.vm-backend2.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_backend3" {
+  value = yandex_compute_instance.vm-backend3.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_ctrl" {
+  value = yandex_compute_instance.vm-ctrl.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_ha" {
+  value = yandex_compute_instance.vm-ha.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_backend1" {
+  value = yandex_compute_instance.vm-backend1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_backend2" {
+  value = yandex_compute_instance.vm-backend2.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_backend3" {
+  value = yandex_compute_instance.vm-backend3.network_interface.0.nat_ip_address
+}
